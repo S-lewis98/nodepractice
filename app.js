19f14939e92eabe22834f75af539c712311d90ae
LEWIS <sl0967451@otc.edu>
2022-11-29 08:46:39 -0600

    readme

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
 console.log("I am happy.")
+
+// git init 
+// git add * 
+// git commit -m ""
+// git remote add origin https://gith.......
+// git push -u origin master
+
+// git add*
+// git commit -m ""
+// git push
+
+
